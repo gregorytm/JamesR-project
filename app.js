b8397a0f1ab261507d2c0d396adb354aadaee976
Gregorytm <gregorytmarsh@.com>
2019-09-07 11:22:14 -0700

    comment out app.listen

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get("/contact", (req, res) => {
 	res.render("james_roberts/routes/contact");
 });
 
-app.listen(3000, () =>{
-	console.log("server listening on port 3000");
-});
+// app.listen(3000, () =>{
+// 	console.log("server listening on port 3000");
+// });
